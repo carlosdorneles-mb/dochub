53862160a88e935ac0711917e84e7d2b9b4d2b03
Carlos Dorneles <carlos.dorneles@mb.com.br>
2025-05-12 23:57:30 -0300

    feat: add service worker support and manifest for PWA capabilities

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,19 @@
-import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {ApplicationConfig, provideZoneChangeDetection, isDevMode} from '@angular/core';
 import {provideRouter, withHashLocation} from '@angular/router';
 import {provideHttpClient} from '@angular/common/http';
 import {provideAnimations} from '@angular/platform-browser/animations';
 
 import {routes} from './app.routes';
+import {provideServiceWorker} from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideAnimations(),
     provideRouter(routes, withHashLocation()),
-    provideHttpClient(),
+    provideHttpClient(), provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ]
 };
