c63a35249f8eeaa22bff90dcb26d4482546a6d5f
Carlos Dorneles <carlos.dorneles@mb.com.br>
2025-05-16 00:02:59 -0300

    feat: add rotating logo animation to footer component

--- src/app/components/shared/footer/footer.component.ts
+++ src/app/components/shared/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {environment} from '@env/environment';
 
 @Component({
@@ -8,7 +8,15 @@ import {environment} from '@env/environment';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
   currentYear: number = new Date().getFullYear();
   repository = environment.repository;
+  rotate = false;
+
+  ngOnInit() {
+    setInterval(() => {
+      this.rotate = true;
+      setTimeout(() => this.rotate = false, 1000); // duração da animação: 1s
+    }, 10000); // a cada 10s
+  }
 }
