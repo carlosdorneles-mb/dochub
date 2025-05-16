5ca499b63edee920d86027869da65d82b728116b
Carlos Dorneles <carlos.dorneles@mb.com.br>
2025-05-16 00:25:03 -0300

    feat: add versioning to web manifest and automate updates in build process

---
+++ scripts/update-manifest-version.js
@@ -0,0 +1,21 @@
+const jsonfile = require('jsonfile');
+const manifestPath = './public/manifest.webmanifest';
+
+const newVersion = process.env.npm_package_version; // Pega a versão do package.json
+
+jsonfile.readFile(manifestPath, (err, manifest) => {
+  if (err) {
+    console.error('Error reading manifest:', err);
+    process.exit(1);
+  }
+
+  manifest.version = newVersion;
+
+  jsonfile.writeFile(manifestPath, manifest, {spaces: 2}, (err) => {
+    if (err) {
+      console.error('Error writing manifest:', err);
+      process.exit(1);
+    }
+    console.log(`Manifest version updated to ${newVersion}`);
+  });
+});
