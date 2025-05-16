f1d2b8002d194de29f7a0c73711f737002f21e7f
Carlos Dorneles <carlos.dorneles@mb.com.br>
2025-05-15 23:44:00 -0300

    feat: add toggle functionality for additional content in section component

--- src/app/components/shared/section/section.component.ts
+++ src/app/components/shared/section/section.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
+import {trigger, transition, style, animate} from '@angular/animations';
+import {NgIf} from '@angular/common';
 
 @Component({
   standalone: true,
   selector: 'app-section',
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './section.component.html',
-  styleUrl: './section.component.scss'
+  styleUrl: './section.component.scss',
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({opacity: 0, transform: 'translateY(20px)'}),
+        animate('400ms ease', style({opacity: 1, transform: 'translateY(0)'}))
+      ]),
+      transition(':leave', [
+        animate('300ms ease', style({opacity: 0, transform: 'translateY(20px)'}))
+      ])
+    ])
+  ]
 })
-export class SectionComponent {
+export class SectionComponent implements OnInit {
+  showMore = false;
+  isDesktop = false;
 
+  @HostListener('window:resize')
+  onResize() {
+    this.isDesktop = window.innerWidth >= 960;
+  }
+
+  ngOnInit() {
+    this.isDesktop = window.innerWidth >= 960;
+  }
 }
