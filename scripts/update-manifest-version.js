7f57cd0ec29a6d4a6893fe6f1dd33f5633fa1c28
Carlos Dorneles <carlos.dorneles@mb.com.br>
2025-05-21 16:53:22 -0300

    feat: update versioning in manifest and ngsw-config files

--- scripts/update-manifest-version.js
+++ scripts/update-manifest-version.js
@@ -1,11 +1,32 @@
 const fs = require('fs');
 const jsonfile = require('jsonfile');
-const manifestPath = './public/manifest.webmanifest';
 
+const manifestPath = './public/manifest.webmanifest';
+const ngswConfigPath = './ngsw-config.json';
 const newVersion = process.env.npm_package_version; // Pega a versão do package.json
 
+// Atualiza a versão no arquivo version.ts
 fs.writeFileSync('src/environments/version.ts', `export const version = '${newVersion}';\n`);
 
+// Atualiza a versão no arquivo ngsw-config.json
+jsonfile.readFile(ngswConfigPath, (err, ngswConfig) => {
+  if (err) {
+    console.error('Error reading ngsw-config.json:', err);
+    process.exit(1);
+  }
+
+  ngswConfig.appData.version = newVersion;
+
+  jsonfile.writeFile(ngswConfigPath, ngswConfig, {spaces: 2}, (err) => {
+    if (err) {
+      console.error('Error writing ngsw-config.json:', err);
+      process.exit(1);
+    }
+    console.log(`ngsw-config.json version updated to ${newVersion}`);
+  });
+});
+
+// Atualiza a versão no arquivo manifest.webmanifest
 jsonfile.readFile(manifestPath, (err, manifest) => {
   if (err) {
     console.error('Error reading manifest:', err);
